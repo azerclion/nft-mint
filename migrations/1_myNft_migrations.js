53c54ef0653483fef4b359120f618978acf1d1ff
azerc <zizimoos@likelion.org>
2022-12-24 22:42:08 +0900

    test done

---
+++ migrations/1_myNft_migrations.js
@@ -0,0 +1,15 @@
+const myNft = artifacts.require("myNFT");
+
+module.exports = function(deployer) {
+  deployer.deploy(
+    myNft,
+    "mynft",
+    "symbol",
+    10,
+    /* global BigInt */
+    1000_000_000_000_000_000n,
+    15,
+    15,
+    "ipfs://QmTzHTBRmt84u81GDZbvRwArcxWVv9awBH9C7AMiZiWNWp"
+  );
+};
